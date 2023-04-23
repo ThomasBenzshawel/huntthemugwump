563ed0d37542ea6e2f541a8172c085e7b0a3afce
Thomas Benzshawel <benzshawelt@msoe.edu>
2023-04-22 20:25:37 -0500

    Program closes!

--- main.cpp
+++ main.cpp
@@ -3,8 +3,18 @@
 #include <string>
 using namespace std;
 
+
+void processResponse(char response){
+    if(tolower(response) == 'q'){
+        exit(EXIT_SUCCESS);
+    }
+}
+
 int main(){
-cout << "wow this works!" << endl;
+cout << " Action: N)orth, S)outh, E)ast, W)est, shoot A)rrow, H)elp, Q)uit:";
+string response;
+cin >> response;
+processResponse(response[0]);
 
 return 0;
 }
